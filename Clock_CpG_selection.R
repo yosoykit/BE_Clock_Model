2c170fd53ac5c9c36beeeeac6f3d6ac5576fbb58
Kit Curtius <Kcurtius@medicine.washington.edu>
2015-12-10 13:33:34 -0800

    intro to selection

---
+++ Clock_CpG_selection.R
@@ -0,0 +1,5 @@
+######################################################################
+#	This script contains the statistical pipeline used to select the #
+#    the set of 67 BE clock CpGs used in the Bayesian model			 #
+#																	 #
+######################################################################
