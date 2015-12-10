ab66b2129afed065a562fad202ddb68afe2ad0a6
Kit Curtius <Kcurtius@medicine.washington.edu>
2015-12-10 13:42:32 -0800

    minfi add

--- Clock_CpG_selection.R
+++ Clock_CpG_selection.R
@@ -1,5 +1,12 @@
 ########################################################
-#	This script contains the statistical  		 #
-#          pipeline used to select the			 #
+#	This script contains the statistical  	  #
+#          pipeline used to select the		  #
 # set of 67 BE clock CpGs used in the Bayesian model   #
 ########################################################
+
+
+### Use functions from package 'minfi' for analysis of methylation data
+source("http://bioconductor.org/biocLite.R")
+biocLite("methylumi")
+biocLite("minfi")
+biocLite("siggenes")
